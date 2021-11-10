0039be1b9b7ab7b7f98a9bb30ddc25945c639dcc
vvv9912 <38299755+vvv9912@users.noreply.github.com>
2021-11-10 14:47:51 +0300

    Update testplot.m

--- test/script/testplot.m
+++ test/script/testplot.m
@@ -1,16 +1,23 @@
 clear
-file = "..\testCickleGalileo.txt";
-data =importdata(file);
+file = "..\Galileo_H.txt";
+data = importdata(file);
 %1 высота ;%ско по высоте
-MU = 0; SIGMA = data(:,2);
-X = -3*SIGMA:0.01:3*SIGMA; %должно быть n массивов по длине от -3 до 3
-%  Z = normcdf(X,MU,SIGMA);
-% figure (1)
-%  plot(X,Z)
-%  figure (2)
- sigma = SIGMA;
- x = X;
- M0= 0; %от высоты зависимость
- f = (1./(sigma.*sqrt(2*pi))).*exp((-(x-M0).^2)./(2*sigma.^2));
- %%for i = 1:length(f)
-  % plot(x(1,:),f)
+
+sigmax = data(:,2);
+sigmay = data(:,3);
+sigma = sqrt(sigmax.^2+sigmay.^2)
+x = -3*sigma:0.01:3*sigma; %должно быть n массивов по длине от -3 до 3
+M0 = data(:,1);
+% M0= 0; %от высоты зависимость
+ for i = 1:length(data(:,2))
+ x = (-3*sigma+M0(i)):0.01:(3*sigma+M0(i));
+ f(i,:) = (1./(sigma(i).*sqrt(2*pi))).*exp((-(x-M0(i)).^2)./(2*sigma(i).^2));
+ end
+ figure (1)
+for i = 1:length(f(:,1))
+   x = (-3*sigma+M0(i)):0.01:(3*sigma+M0(i));
+   plot(x(1,:),f(i,:))
+   hold on;
+   grid on;
+end
+%   f = (1./(sigma.*sqrt(2*pi))).*exp((-(x-0).^2)./(2*sigma.^2));
